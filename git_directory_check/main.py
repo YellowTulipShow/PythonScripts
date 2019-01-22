51984dc198f70c11c1156cd973d60ec0d460331e
YellowTulipShow <1426689530@qq.com>
2019-01-22 11:06:27 +0800

    git_directory_check dev complete!

--- git_directory_check/main.py
+++ git_directory_check/main.py
@@ -9,39 +9,20 @@ from subprocess import Popen, PIPE
 def read_config_json_file():
     f = open("config.json", encoding='utf-8')
     return json.load(f);
+
 # 读取 json 配置文件
 c = read_config_json_file();
 
-def is_window_path(path):
-    return re.match(r"[a-zA-Z]:\\.*", path) != None
+def get_root_paths():
+    roots = c["def_check_paths"]
 
-def to_window_path(path):
-    m = re.compile(r"([a-zA-Z]):\\(.*)", re.I | re.M | re.U)
-    r = m.findall(path)[0]
-    drive_letter = r[0]
-    son_path = re.sub(r"\\+", "/", str(r[1]))
-    return "/{}/{}".format(drive_letter, son_path)
+    if len(roots) <= 0:
+        roots.append(os.getcwd())
 
-def get_root_path():
-    # 自定义的默认的目录地址
-    root = c["default_root_path"]
-    if not root:
-        # 获取当前目录地址
-        root = os.getcwd()
     if len(sys.argv) > 1:
-        root = sys.argv[1]
-    # if is_window_path(root):
-    #     root = to_window_path(root)
-    return root;
+        roots = [sys.argv[1]];
 
-def is_git_directory(root):
-    if os.path.isfile(root):
-        return False
-    return ".git" in os.listdir(root)
-
-# start program:
-root = get_root_path()
-print("root:", root)
+    return roots;
 
 def is_ignore_path(root):
     for ignore in c["ignore_paths"]:
@@ -49,29 +30,36 @@ def is_ignore_path(root):
             return True
     return False;
 
-def git_status():
-    return Popen(["git", "status"], stdout=PIPE).communicate()[0].decode("utf-8")
+def exe_command(str_command):
+    # http://www.cnblogs.com/nerrissa/articles/5784746.html
+    p = Popen(str_command, shell=True, stdout=PIPE, stderr=PIPE)
+    p.wait()
+    out = p.stdout.read();
+    j = {
+        "out": out.decode("utf-8"),
+    }
+    return j
 
-def get_repositories_status_infos(root):
-    print(root)
+def git_status():
+    return
+
+def get_repositories_status_info(root):
     os.chdir(root)
     info = {
         "path": root,
-        # "is_need_commit": git_status()
+        "out": exe_command("git status")["out"],
     }
     return info
 
-son_count = 0
-def GetAllSonPathGitInfos(root):
-    global son_count
-    status_infos = []
+def get_all_git_repositories(root):
+    rep_infos = []
     if os.path.isfile(root):
-        return status_infos
+        return rep_infos
 
     if ".git" in os.listdir(root):
-        info = get_repositories_status_infos(root)
-        status_infos.append(info)
-        return status_infos
+        info = get_repositories_status_info(root)
+        rep_infos.append(info)
+        return rep_infos
 
     # 当前路径不是 git 存储库
     folders = os.listdir(root)
@@ -80,61 +68,54 @@ def GetAllSonPathGitInfos(root):
 
         if is_ignore_path(path):
             continue
-        print(path)
 
-        silist = GetAllSonPathGitInfos(path)
+        silist = get_all_git_repositories(path)
         if len(silist) > 0:
             for i in silist:
-                status_infos.append(i)
-    return status_infos
+                rep_infos.append(i)
+    return rep_infos
 
+def filter_infos(infos):
+    status_codes = [
+        "Changes not staged for commit:",
+        "Changes to be committed:",
+        "(use \"git push\" to publish your local commits)"
+    ]
 
-status_infos = GetAllSonPathGitInfos(root)
-# for info in status_infos:
-#     print(info)
-print("son_count:", son_count)
+    rinfos = []
+    for info in infos:
+        for code in status_codes:
+            if code in info["out"]:
+                rinfos.append(info)
+                break
+    return rinfos
 
-# # RadoRado dev code:
-# def is_git_directory(root):
-#     if os.path.isfile(root):
-#         return False
+def font_red(str_text):
+    # http://www.cnblogs.com/ping-y/p/5897018.html
+    return "\033[1;31m{}\033[0m".format(str_text)
 
-#     return ".git" in os.listdir(root)
+def print_list_infos(infos):
+    for info in infos:
+        print("path: {}".format(font_red(info["path"])))
+        print("out: \n{}".format(info["out"]))
+        print("-" * 80 + "\n")
 
-# def git_status():
-#     return Popen(["git", "status"], stdout=PIPE).communicate()[0].decode("utf-8")
+# start program:
+for root in get_root_paths():
+    rep_infos = []
 
-# def collect_git_statuses(root):
-#     result = {
-#         "git": {},
-#         "notgit": []
-#     }
+    try:
+        # 获取目录下所有仓库信息
+        rep_infos = get_all_git_repositories(root)
 
-#     if is_git_directory(root):
-#         print("Root is a git directory")
-#         return
+    except Exception as e:
+        print("root: {} Error! continue...".format(root))
+        print("-" * 80 + "\n")
+        continue
 
-#     folders = os.listdir(root)
+    else:
+        # 筛选过滤需要展示的仓库信息
+        need_show_info = filter_infos(rep_infos)
 
-#     for folder in folders:
-#         path = os.path.join(root, folder)
-#         if is_git_directory(path):
-#             os.chdir(path)
-#             result["git"][path] = git_status()
-#         else:
-#             result["notgit"].append(path)
-
-#     return result
-
-# all_folders = collect_git_statuses(root)
-# repos = all_folders["git"]
-# not_repos = all_folders["notgit"]
-
-# repos = [r for r in repos if "nothing to commit" not in repos[r]]
-# print("Git repos with git status: {}".format(len(repos)))
-# for repo in repos:
-#     print(repo)
-
-# print("Not git repos: {}".format(len(not_repos)))
-# for folder in not_repos:
-#     print(folder)
+        # 打印仓库信息
+        print_list_infos(need_show_info)
