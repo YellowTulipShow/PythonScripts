833838d2b049a0b9929402974116a9af5e8209ae
YellowTulipShow <1426689530@qq.com>
2019-03-23 14:56:42 +0800

    Decomposition parameter

--- random/password.py
+++ random/password.py
@@ -1,14 +1,22 @@
 # coding: UTF-8
 import random
 
+nmin = 11
+mmax = 19
+
 sl = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+
+sl = '0123456789'
+sl += 'abcdefghijklmnopqrstuvwxyz'
+# sl += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# sl += '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
 ## 输出 ASCII 常用字符编码表
 # for i in range(0, len(sl)):
 #     print("i:", i+33, "c:", sl[i])
 
 sl = [ c for c in sl]
-rl = random.randint(11, 19)
+rl = random.randint(nmin, mmax)
 sl = random.sample(sl, rl)
 sl = ''.join(sl)
 
